1783e06eee7cd18e2bfe9f72632ebe1bb6a21bcc
korah91 <62379054+korah91@users.noreply.github.com>
2022-05-11 11:56:41 +0200

    .

--- React/src/App.js
+++ React/src/App.js
@@ -1,14 +1,32 @@
 import React, {Component} from 'react'
 import Table from './Table'
+import Form from './Form'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Table />
-      </div>
-    )
-  }
+    state = {
+        characters: [],
+    }
+
+    removeCharacter = (index) => {
+        const {characters} = this.state
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index
+            }),
+        })
+    }
+
+    render() {
+        const {characters} = this.state
+
+        return (
+            <div className='container'>
+                <Table characterData={characters} removeCharacter={this.removeCharacter} />
+                <Form />
+            </div>
+        )
+    }
 }
 
 export default App
--- React/src/Table.js
+++ React/src/Table.js
@@ -1,4 +1,4 @@
-import React, {Component   } from 'react'
+import React, {Component} from 'react'
 
 const TableHeader = () => {
     return (
@@ -10,21 +10,30 @@ const TableHeader = () => {
         </thead>
     )
 }
-const TableBody = () => {
-    return (
-        <tbody/>
-    )
+const TableBody = (props) => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
+            </tr>
+        )
+    })
+    return <tbody>{rows}</tbody>
 }
 
-class Table extends Component{
-    render(){
-        return (
-            <table>
-                <TableHeader/>
-                <TableBody/>
-            </table>
-        )
-    }
+const Table = (props) => {
+    const {characterData, removeCharacter} = props
+
+    return (
+        <table>
+            <TableHeader />
+            <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+        </table>
+    )
 }
 
 
